70dc5e755371981df7b8f4e79a3e2e5d7275473c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-06-08 17:44:24 -0700

    .

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
-(define collection #"sorawee-test")
+(define collection "sorawee-test")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/sorawee-test.scrbl" ())))
